f1ed8f3db7357434bce3165c8746134b10f65099
Luke Taylor <77284962+luke-taylor@users.noreply.github.com>
2024-06-21 13:01:55 +0000

    added configure method

--- internal/provider/coffees_data_source.go
+++ internal/provider/coffees_data_source.go
@@ -1,17 +1,18 @@
 package provider
 
 import (
-  "context"
+	"context"
+	"fmt"
 
-  "github.com/hashicorp-demoapp/hashicups-client-go"
-  "github.com/hashicorp/terraform-plugin-framework/datasource"
-  "github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp-demoapp/hashicups-client-go"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 )
-
 
 // Ensure the implementation satisfies the expected interfaces.
 var (
-	_ datasource.DataSource = &coffeesDataSource{}
+	_ datasource.DataSource              = &coffeesDataSource{}
+	_ datasource.DataSourceWithConfigure = &coffeesDataSource{}
 )
 
 // NewCoffeesDataSource is a helper function to simplify the provider implementation.
@@ -36,4 +37,25 @@ func (d *coffeesDataSource) Schema(_ context.Context, _ datasource.SchemaRequest
 
 // Read refreshes the Terraform state with the latest data.
 func (d *coffeesDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+}
+
+// Configure adds the provider configured client to the data source.
+func (d *coffeesDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	// Add a nil check when handling ProviderData because Terraform
+	// sets that data after it calls the ConfigureProvider RPC.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*hashicups.Client)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *hashicups.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	d.client = client
 }
