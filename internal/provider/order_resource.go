31cd4f17f1221327651b544777de072710350167
luke-taylor <lukejamestaylor98@outlook.com>
2024-07-01 15:59:13 +0000

    add new resource

---
+++ internal/provider/order_resource.go
@@ -0,0 +1,47 @@
+package provider
+
+import (
+    "context"
+
+    "github.com/hashicorp/terraform-plugin-framework/resource"
+    "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+)
+
+// Ensure the implementation satisfies the expected interfaces.
+var (
+    _ resource.Resource = &orderResource{}
+)
+
+// NewOrderResource is a helper function to simplify the provider implementation.
+func NewOrderResource() resource.Resource {
+    return &orderResource{}
+}
+
+// orderResource is the resource implementation.
+type orderResource struct{}
+
+// Metadata returns the resource type name.
+func (r *orderResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+    resp.TypeName = req.ProviderTypeName + "_order"
+}
+
+// Schema defines the schema for the resource.
+func (r *orderResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+    resp.Schema = schema.Schema{}
+}
+
+// Create creates the resource and sets the initial Terraform state.
+func (r *orderResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+}
+
+// Read refreshes the Terraform state with the latest data.
+func (r *orderResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+}
+
+// Update updates the resource and sets the updated Terraform state on success.
+func (r *orderResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+}
+
+// Delete deletes the resource and removes the Terraform state on success.
+func (r *orderResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+}
