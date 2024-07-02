42128cc48bd2116e6f6e6265e4604ad1254afb32
luke-taylor <lukejamestaylor98@outlook.com>
2024-07-02 19:30:12 +0000

    add configure method

--- internal/provider/order_resource.go
+++ internal/provider/order_resource.go
@@ -2,15 +2,19 @@ package provider
 
 import (
     "context"
-
+    "fmt"
+    "github.com/hashicorp-demoapp/hashicups-client-go"
     "github.com/hashicorp/terraform-plugin-framework/resource"
     "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 )
 
+
 // Ensure the implementation satisfies the expected interfaces.
 var (
-    _ resource.Resource = &orderResource{}
+    _ resource.Resource              = &orderResource{}
+    _ resource.ResourceWithConfigure = &orderResource{}
 )
+
 
 // NewOrderResource is a helper function to simplify the provider implementation.
 func NewOrderResource() resource.Resource {
@@ -18,7 +22,9 @@ func NewOrderResource() resource.Resource {
 }
 
 // orderResource is the resource implementation.
-type orderResource struct{}
+type orderResource struct {
+    client *hashicups.Client
+}
 
 // Metadata returns the resource type name.
 func (r *orderResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
@@ -29,6 +35,29 @@ func (r *orderResource) Metadata(_ context.Context, req resource.MetadataRequest
 func (r *orderResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
     resp.Schema = schema.Schema{}
 }
+
+// Configure adds the provider configured client to the resource.
+func (r *orderResource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+    // Add a nil check when handling ProviderData because Terraform
+    // sets that data after it calls the ConfigureProvider RPC.
+    if req.ProviderData == nil {
+          return
+      }
+  
+      client, ok := req.ProviderData.(*hashicups.Client)
+  
+      if !ok {
+          resp.Diagnostics.AddError(
+              "Unexpected Data Source Configure Type",
+              fmt.Sprintf("Expected *hashicups.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+          )
+  
+          return
+      }
+  
+      r.client = client
+  }
+  
 
 // Create creates the resource and sets the initial Terraform state.
 func (r *orderResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
@@ -45,3 +74,4 @@ func (r *orderResource) Update(ctx context.Context, req resource.UpdateRequest, 
 // Delete deletes the resource and removes the Terraform state on success.
 func (r *orderResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
 }
+
