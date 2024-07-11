d4930279550163b4f58c851b069cdb68749cba1a
luke-taylor <lukejamestaylor98@outlook.com>
2024-07-11 19:54:10 +0000

    add uppdate method

--- internal/provider/order_resource.go
+++ internal/provider/order_resource.go
@@ -227,8 +227,69 @@ func (r *orderResource) Read(ctx context.Context, req resource.ReadRequest, resp
 	}
 	
 
-// Update updates the resource and sets the updated Terraform state on success.
-func (r *orderResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	func (r *orderResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+    // Retrieve values from plan
+    var plan orderResourceModel
+    diags := req.Plan.Get(ctx, &plan)
+    resp.Diagnostics.Append(diags...)
+    if resp.Diagnostics.HasError() {
+        return
+    }
+
+    // Generate API request body from plan
+    var hashicupsItems []hashicups.OrderItem
+    for _, item := range plan.Items {
+        hashicupsItems = append(hashicupsItems, hashicups.OrderItem{
+            Coffee: hashicups.Coffee{
+                ID: int(item.Coffee.ID.ValueInt64()),
+            },
+            Quantity: int(item.Quantity.ValueInt64()),
+        })
+    }
+
+    // Update existing order
+    _, err := r.client.UpdateOrder(plan.ID.ValueString(), hashicupsItems)
+    if err != nil {
+        resp.Diagnostics.AddError(
+            "Error Updating HashiCups Order",
+            "Could not update order, unexpected error: "+err.Error(),
+        )
+        return
+    }
+
+    // Fetch updated items from GetOrder as UpdateOrder items are not
+    // populated.
+    order, err := r.client.GetOrder(plan.ID.ValueString())
+    if err != nil {
+        resp.Diagnostics.AddError(
+            "Error Reading HashiCups Order",
+            "Could not read HashiCups order ID "+plan.ID.ValueString()+": "+err.Error(),
+        )
+        return
+    }
+
+    // Update resource state with updated items and timestamp
+    plan.Items = []orderItemModel{}
+    for _, item := range order.Items {
+        plan.Items = append(plan.Items, orderItemModel{
+            Coffee: orderItemCoffeeModel{
+                ID:          types.Int64Value(int64(item.Coffee.ID)),
+                Name:        types.StringValue(item.Coffee.Name),
+                Teaser:      types.StringValue(item.Coffee.Teaser),
+                Description: types.StringValue(item.Coffee.Description),
+                Price:       types.Float64Value(item.Coffee.Price),
+                Image:       types.StringValue(item.Coffee.Image),
+            },
+            Quantity: types.Int64Value(int64(item.Quantity)),
+        })
+    }
+    plan.LastUpdated = types.StringValue(time.Now().Format(time.RFC850))
+
+    diags = resp.State.Set(ctx, plan)
+    resp.Diagnostics.Append(diags...)
+    if resp.Diagnostics.HasError() {
+        return
+    }
 }
 
 // Delete deletes the resource and removes the Terraform state on success.
